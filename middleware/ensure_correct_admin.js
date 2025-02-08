e2798d3f0a16ddf6f6ac895fcc609059749e1545
Indu Singh <indupriyasingh222@gmail.com>
2025-02-08 11:19:31 -0400

    Update ensure_correct_admin.js

--- middleware/ensure_correct_admin.js
+++ middleware/ensure_correct_admin.js
@@ -7,14 +7,8 @@
 const ensureCorrectAdmin = (req, res, next) => {
     const institutionId = req.params.id;
 
-    // Check if the user is logged in and has an admin role
-    if (!req.session || !req.session.user || req.session.user.role !== 'admin') {
-        req.session.errorMessage = "You must be an admin to access this page.";
-        return res.status(403).redirect('/login'); // Redirect unauthorized users to login
-    }
-
     // Verify if the admin belongs to the requested institution
-    if (req.session.user.institutionId != institutionId) {
+    if (req.session.user.role === 'admin' && req.session.user.institutionId != institutionId) {
         return res.status(403).json({ error: `Access denied. This institution does not belong to your account (accessed=${institutionId}, yours=${req.session.user.institution_id}).` });
     }
 
