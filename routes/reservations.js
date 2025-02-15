e3842ee65b5e7be2f220ad31f98f93688745ed4c
Roy Mattar <roy.mattars@gmail.com>
2025-02-15 23:45:39 +0200

    [CLIENT][SERVER] Fix reservationSelection page and GET /reservations route

--- routes/reservations.js
+++ routes/reservations.js
@@ -2,18 +2,67 @@ const express = require('express');
 const router = express.Router();
 const requireAuth = require('../middleware/require_auth');
 
+// ------ Reservations Routes ------
 function reservationsRouter(db) {
-    // ------ Reservation Management Routes ------
     // Route to get all reservations
     router.get('/', requireAuth, function (req, res, next) {
-        db.all(
-            'SELECT reservation_id, user_id, seat_id, start_time, end_time, status FROM reservations',
-            [],
-            function (err, rows) {
-                if (err) return next(err);
-                res.render('reservationManagement', { institutionId: req.session.user.institutionId, reservations: rows });
-            }
-        );
+        if (req.session.user.role === 'admin') {
+            const institutionId = req.session.user.institutionId;
+            db.all(
+                `
+                SELECT r.reservation_id, r.user_id, r.seat_id, r.start_time, r.end_time, r.status
+                FROM reservations r
+                JOIN seats s ON r.seat_id = s.seat_id
+                JOIN spaces sp ON s.space_id = sp.space_id
+                JOIN institutions i ON sp.institution_id = i.institution_id
+                WHERE i.institution_id = ?
+                `,
+                [institutionId],
+                function (err, rows) {
+                    if (err) return next(err);
+                    res.render('reservationManagement', { institutionId, reservations: rows });
+                }
+            );
+        } else {
+            db.all(
+                `
+                SELECT
+                    r.reservation_id,
+                    i.institution_name, sp.space_name, s.seat_name, s.type, s.facilities,
+                    DATE(r.start_time) as date,
+                    TIME(r.start_time) as start_time,
+                    TIME(r.end_time) as end_time,
+                    r.end_time as end_time_date,
+                    r.status
+                FROM reservations r
+                JOIN seats s ON r.seat_id = s.seat_id
+                JOIN spaces sp ON s.space_id = sp.space_id
+                JOIN institutions i ON sp.institution_id = i.institution_id
+                WHERE r.user_id = ?
+                ORDER BY r.start_time DESC
+                `,
+                [req.session.user.id],
+                function (err, rows) {
+                    if (err) return next(err);
+
+                    const now = new Date();
+                    const activeReservations = [];
+                    const pastReservations = [];
+
+                    // Sort reservations into active and past
+                    rows.forEach(res => {
+                        const endTime = new Date(res.end_time_date);
+                        if (res.status === 'active' && endTime > now) { // TODO: change to 'booked'
+                            activeReservations.push(res);
+                        } else {
+                            pastReservations.push(res);
+                        }
+                    })
+
+                    res.render('reservationSelection', { activeReservations, pastReservations });
+                }
+            );
+        }
     });
 
     //Route to create a reservation for a seat
